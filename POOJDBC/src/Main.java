8084f285dc2aa2051416eca393b2e838f11db929
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2023-03-22 09:49:03 -0600

    Creando Form

---
+++ POOJDBC/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+            fomAlumno frm = new fomAlumno();
+            frm.setContentPane(frm.panelPrincipal);
+            frm.setSize(600, 500);
+            frm.setVisible(true);
+
+        System.out.println("Hello world!");
+    }
+}
---
+++ POOJDBC/src/fomAlumno.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class fomAlumno extends JFrame {
+    public JPanel panelPrincipal;
+    private JButton button1;
+}
