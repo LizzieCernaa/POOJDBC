b4b1f87872ad67d7786a87126a5dd5f9cd52538d
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2023-03-24 09:53:33 -0600

    Conectando base de datos Postgres

--- POOJDBC/src/fomAlumno.java
+++ POOJDBC/src/fomAlumno.java
@@ -1,6 +1,34 @@
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class fomAlumno extends JFrame {
     public JPanel panelPrincipal;
     private JButton button1;
+public fomAlumno() {
+    button1.addMouseListener(new MouseAdapter() {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            super.mouseClicked(e);
+
+            String url = "jdbc:postgresql://localhost:5432/test";
+            String UserName = "postgres";
+            String password = "NADA12345";
+            try {
+                Connection conn = DriverManager.getConnection(url, UserName, password);
+                JOptionPane.showMessageDialog(null, "Coneccion Establecida");
+            }catch (Exception ex)
+            {
+                JOptionPane.showMessageDialog(null,"Error tratando de conectarse "+ ex.getMessage());
+            }
+
+
+
+
+        }
+
+    });
+}
 }
