7fcea0ce1f5a032df3572022fe98462b9243041b
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2023-04-22 21:30:11 -0600

    Completando AlumnoRepository

--- POOJDBC/src/AlumnoRepository.java
+++ POOJDBC/src/AlumnoRepository.java
@@ -69,8 +69,90 @@ public class AlumnoRepository {
             return false;
         }
     }
+    public boolean ActualizarAlumno(Alumno alumno)
+    {
+        try {
+            this.Conectar();
+            String sql = "UPDATE alumnos SET fecha_nacimiento =?, nombre=?, apellido =?, telefono =?, direccion =?," +
+                        " estatura =?, peso =?, correo =?, nacionalidad =?, sexo =? WHERE id = ?";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, alumno.getFechaNacimiento());
+            statement.setString(2, alumno.getNombre());
+            statement.setString(3, alumno.getApellido());
+            statement.setString(4, alumno.getTelefono());
+            statement.setString(5, alumno.getDireccion());
+            statement.setDouble(6, alumno.getEstatura());
+            statement.setDouble(7, alumno.getPeso());
+            statement.setString(8, alumno.getCorreo());
+            statement.setString(9, alumno.getNacionalidad());
+            statement.setString(10, alumno.getSexo());
+            statement.setLong(11, alumno.getId());
 
+            int filasActualizadas = statement.executeUpdate();
+            statement.close();
+            this.Desconectar();
+            if (filasActualizadas > 0) {
+                return true;
+            } else {
+                error = "No se pudo actualizar al alumno";
+                return false;
+            }
 
+        }catch (Exception ex)
+        {
+            error= ex.getMessage();
+            return false;
+        }
+    }
+
+    public boolean EliminarAlumno (Long id)
+    {
+        try {
+            this.Conectar();
+            String query = "DELETE FROM alumno WHERE id = ?";
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setLong(1, id);
+            int filasAfectadas = statement.executeUpdate();
+            statement.close();
+            this.Desconectar();
+            if (filasAfectadas > 0) {
+                return true;
+            } else {
+                error = "No se pudo eliminar al alumno";
+                return false;
+            }
+
+        }
+        catch (Exception ex)
+        {
+            error= ex.getMessage();
+            return false;
+        }
+    }
+
+    public boolean EliminarTodosAlumnos ()
+    {
+        try {
+            this.Conectar();
+            String query = "DELETE FROM alumno";
+            PreparedStatement statement = conn.prepareStatement(query);
+            int filasAfectadas = statement.executeUpdate();
+            statement.close();
+            this.Desconectar();
+            if (filasAfectadas > 0) {
+                return true;
+            } else {
+                error = "No se pudieron eliminar a los alumno";
+                return false;
+            }
+
+        }
+        catch (Exception ex)
+        {
+            error= ex.getMessage();
+            return false;
+        }
+    }
 
     public String getError() {
         return error;
